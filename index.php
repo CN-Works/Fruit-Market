3b138b03a1a14e83bad902c3f8c746e6e4221346
ChernyyOrel <92865037+ChernyyWorks@users.noreply.github.com>
2023-05-11 14:59:19 +0200

    Chore : adding a container and fruit selection

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ajouter un produit</title>
+</head>
+<body>
+    <div class="container">
+        <h1>Add a fruit !</h1>
+        <form action="traitement.php" method="post">
+            <p>
+                <label>
+                    Fruit name :
+                    <input type="text" name="name">
+                </label>
+            </p>
+        </form>
+    </div>
+</body>
+</html>
