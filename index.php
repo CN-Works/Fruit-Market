b5274cb7f59609b4cea2c44ccd2b09f172bcce69
ChernyyOrel <92865037+ChernyyWorks@users.noreply.github.com>
2023-05-12 09:53:36 +0200

    Container management & creating box

--- index.php
+++ index.php
@@ -13,33 +13,35 @@
 </head>
 <body>
     <main class="container">
-        <!-- Title -->
-        <h1>Add a fruit</h1>
+        <div class="central-box">
+            <!-- Title -->
+            <h1 class="main-title">Add a fruit !</h1>
 
-        <!-- Global form -->
-        <form action="traitement.php" method="post">
-            <!-- Fruit selection -->
-            <div class="input-section">
-                <p class="input-title">Fruit name :</p>
-                <input type="text" name="name">
-            </div>
+            <!-- Global form -->
+            <form class="input-section" action="traitement.php" method="post">
+                <!-- Fruit selection -->
+                <div class="inputs">
+                    <p class="input-title">Fruit name :</p>
+                    <input class="input-button" type="text" name="name">
+                </div>
 
-            <!-- Setting a price -->
-            <div class="input-section">
-                <p class="input-title">Fruit price :</p>
-                <input type="number" step="any" name="price">
-            </div>
+                <!-- Setting a price -->
+                <div class="inputs">
+                    <p class="input-title">Fruit price :</p>
+                    <input type="number" step="any" name="price">
+                </div>
 
-            <!-- Choosing a quantity -->
-            <div class="input-section">
-                <p class="input-title">Quantity :</p>
-                <input type="number" name="quantity" value="1">
-            </div>
+                <!-- Choosing a quantity -->
+                <div class="inputs">
+                    <p class="input-title">Quantity :</p>
+                    <input type="number" name="quantity" value="1">
+                </div>
 
-            <!-- Submit button -->
-            <input type="submit" name="submit" value="Add fruit">
-            
-        </form>
+                <!-- Submit button -->
+                <input class="submit-button" type="submit" name="submit" value="Add fruit">
+                
+            </form>
+        </div>
     </main>
 </body>
 </html>
