f23ff28504c6cd0ace30d77ca8a606909cfc4b0d
ChernyyOrel <chernyyorel.all@gmail.com>
2023-05-16 13:47:31 +0200

    Chore : adding more classes for style

--- cart.php
+++ cart.php
@@ -29,11 +29,11 @@
                             "<table class='table-main'>",
                                 "<thead>",
                                     "<tr>",
-                                        "<th class='table-header-title'>#</th>",
-                                        "<th class='table-header-title'>Name</th>",
-                                        "<th class='table-header-title'>Price/unit</th>",
-                                        "<th class='table-header-title'>Quantity</th>",
-                                        "<th class='table-header-title'>Total</th>",
+                                        "<th class='table-header-title id'>#</th>",
+                                        "<th class='table-header-title name'>Name</th>",
+                                        "<th class='table-header-title price'>Price/unit</th>",
+                                        "<th class='table-header-title quantity'>Quantity</th>",
+                                        "<th class='table-header-title total'>Total</th>",
                                     "</tr>",
                                 "</thead>",
                                 "<tbody>";
@@ -41,11 +41,11 @@
                     // Adding items
                     foreach($_SESSION["cart"] as $index => $product) {
                         echo    "<tr>",
-                                    "<td>".$index."</td>",
-                                    "<td>".$product["name"]."</td>",
-                                    "<td>".$product["price"]."$</td>",
-                                    "<td>".$product["quantity"]."</td>",
-                                    "<td>".$product["total"]."$</td>",
+                                    "<td class='id'>".$index."</td>",
+                                    "<td class='name'>".$product["name"]."</td>",
+                                    "<td class='price'>".$product["price"]."$</td>",
+                                    "<td class='quantity'>".$product["quantity"]."</td>",
+                                    "<td class='total'>".$product["total"]."$</td>",
                                 "</tr>";
                     }
 
