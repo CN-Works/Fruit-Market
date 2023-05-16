878d93032189bb7b1113416dc7f208bd1d706247
ChernyyOrel <chernyyorel.all@gmail.com>
2023-05-16 09:45:19 +0200

    Adding cart default page

--- cart.php
+++ cart.php
@@ -9,13 +9,18 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>All fruits - Market</title>
+    <link rel="stylesheet" href="./css/recap.css">
 </head>
 <body>
-    
+    <main class="container">
+        <div class="central-box">
+            <p>test</p>
+            <!-- <pre>
+                <?php
+                    var_dump($_SESSION);
+                ?>
+            </pre> -->
+        </div>
+    </main> 
 </body>
-    <pre>
-        <?php
-            var_dump($_SESSION);
-        ?>
-    </pre>
 </html>
