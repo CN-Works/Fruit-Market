02c50746ca79cfeff402d47497bf73fdd230333d
ChernyyOrel <chernyyorel.all@gmail.com>
2023-05-16 10:22:46 +0200

    Fix : css file name

--- cart.php
+++ cart.php
@@ -9,12 +9,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>All fruits - Market</title>
-    <link rel="stylesheet" href="./css/recap.css">
+    <link rel="stylesheet" href="./css/cart.css">
 </head>
 <body>
     <main class="container">
         <div class="central-box">
-            <p>test</p>
+            <p>Si tu vois ça c'est que ça marche (vite fait)</p>
             <!-- <pre>
                 <?php
                     var_dump($_SESSION);
