2121e45b01395fd5f998145335390a7023038e0a
ChernyyOrel <chernyyorel.all@gmail.com>
2023-05-16 09:09:43 +0200

    Security : filter input verifications

--- traitement.php
+++ traitement.php
@@ -2,7 +2,11 @@
     session_start();
 
     if (isset($_POST["submit"])) {
-
+        
+        // Checking inputs
+        $name = filter_input(INPUT_POST, "name",FILTER_SANITIZE_STRING);
+        $price = filter_input(INPUT_POST,"price",FILTER_VALIDATE_FLOAT,FILTER_FLAG_ALLOW_FRACTION);
+        $quantity = filter_input(INPUT_POST,"quantity",FILTER_VALIDATE_INT);
     }
 
     header("Location:index.php")
