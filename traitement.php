23f3f495dcc50bd55eeb6ea897e1736c5aeec8a6
ChernyyOrel <chernyyorel.all@gmail.com>
2023-05-23 16:33:44 +0200

    Explaining my code

--- traitement.php
+++ traitement.php
@@ -1,10 +1,26 @@
 <?php
+    // Starting session to create an user unique data container
+    // In our case, the server will store user's cart information (quantity, items ect..)
     session_start();
+
+
+    // Native global variables are php variables you can access anywhere in the project (any files).
+    // Some of them are very useful as $_SESSION wich exists when session is started
+    // -and stores any data related with user.
+    // Native global variables are tables (key --> value)
+
+
+    // Cookies works as text file with hashed content in it, 
+    //it can be use to locally store data for future usage (ex: darkmode, passwords..)
+
 
     // Getting post request data
     if (isset($_POST["submit"])) {
         
         // Checking user inputs using filters, avoid xss & code injections
+        xss , comment ça fonctionne et les dangers 
+        autre moyens que les filter input 
+        comment marchent les filter input
         $name = filter_input(INPUT_POST, "name",FILTER_SANITIZE_STRING);
         $price = filter_input(INPUT_POST,"price",FILTER_VALIDATE_FLOAT,FILTER_FLAG_ALLOW_FRACTION);
         $quantity = filter_input(INPUT_POST,"quantity",FILTER_VALIDATE_INT);
@@ -20,6 +36,7 @@
             );
 
             // Saving the new product data in session
+            // works like an "array-push" in the cart table
             $_SESSION["cart"][] = $new_fruit;
         }
     }
