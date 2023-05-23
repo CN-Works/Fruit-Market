6022412987b45178045e19ef883de36d80c42064
ChernyyOrel <chernyyorel.all@gmail.com>
2023-05-23 14:19:50 +0200

    Style : form design & comments

--- traitement.php
+++ traitement.php
@@ -4,12 +4,12 @@
     // Getting post request data
     if (isset($_POST["submit"])) {
         
-        // Checking user inputs using filters
+        // Checking user inputs using filters, avoid xss & code injections
         $name = filter_input(INPUT_POST, "name",FILTER_SANITIZE_STRING);
         $price = filter_input(INPUT_POST,"price",FILTER_VALIDATE_FLOAT,FILTER_FLAG_ALLOW_FRACTION);
         $quantity = filter_input(INPUT_POST,"quantity",FILTER_VALIDATE_INT);
 
-        // Checking if inputs are correct
+        // Checking if inputs have any values (no null)
         if ($name && $price && $quantity) {
             // Stacking in array
             $new_fruit = array(
